8530f5e8694e753fc858919443cbca8d7c2c7eb8
samdotson1992 <samdotson1992@gmail.com>
2017-11-14 09:10:51 -0600

    modify sign up

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,7 @@ function openTab(evt, TabName) {
 
 
  function signUp() {
+     console.log("sign up is running")
 
      var obj = {
          'username_signup': document.getElementById('username_signup').value,
@@ -27,10 +28,7 @@ function openTab(evt, TabName) {
          alert("Passwords don't match!")
      } else {
          var data = JSON.stringify(obj)
-         return data
          console.log(data)
-     }
-
 
      $.ajax({
          type: "POST",
@@ -46,7 +44,7 @@ function openTab(evt, TabName) {
          }
      });
 
- }
+ }}
 
  function signIn() {
      var data = JSON.stringify({
